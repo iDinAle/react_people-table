dcc61b2f0c587a56cd1eda09ae4bea7abb3f2062
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-10 15:55:55 +0200

    Updates

---
+++ precache-manifest.28231152e7c60f5c36b2866b0e55c637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549d673a8760a252e6378051be61962c",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "41a39fd07c3935b7d5ef",
+    "url": "/react_people-table/static/css/main.de7154a5.chunk.css"
+  },
+  {
+    "revision": "3485fa9145fb43f72c73",
+    "url": "/react_people-table/static/js/2.506750e1.chunk.js"
+  },
+  {
+    "revision": "41a39fd07c3935b7d5ef",
+    "url": "/react_people-table/static/js/main.e67df78e.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  },
+  {
+    "revision": "c8c3420f7c7c003a97d346ca33007ec6",
+    "url": "/react_people-table/static/media/light-gray.c8c3420f.jpg"
+  }
+]);
